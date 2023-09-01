6a6c0095151776062ae0925692b7a24c3b11af30
pengsun <3272152577@qq.com>
2023-09-01 19:52:29 +0800

    完善获取内存容量的另外两种方法,并实验成功,添加了获取失败的处理,并添加了屏幕显示调试信息

--- boot/setup.s
+++ boot/setup.s
@@ -6,6 +6,9 @@ STACK_TOP_ADDR equ SETUP_BASE_ADDR
 
     ; 跳转至setup入口进行启动保护模式的准备
     jmp setup_start
+
+    ;----------------------------------------------- 数据区域 ----------------------------------------------------
+
     ; 定义GDT相关数据结构
     GDT_BASE:   dd 0x00000000
                 dd 0x00000000       ; 第0个段描述符（不可用）
@@ -31,7 +34,7 @@ STACK_TOP_ADDR equ SETUP_BASE_ADDR
     PTR_GDT   dw GDT_LIMIT
               dd GDT_BASE                         ; GDTR指针变量
 
-    Physical_memory_capacity dd 0                 ; 物理内存容量0xb00
+    Physical_memory_capacity dd 0                 ; 物理内存容量访问地址0xb09
     Ards_buff dd 0                                ; Ards结构体缓存
               dd 0
               dd 0
@@ -39,12 +42,18 @@ STACK_TOP_ADDR equ SETUP_BASE_ADDR
               dd 0
     Ards_cnt  dw 0                                ; Ards结构体返回个数
 
-    setup_string_msg db 'ready in protect mode...' ; msg信息打印
-    two_get_mem_string db 'get mem failed'         ; 
-; 准备进入保护模式
+    setup_string_msg db 'get memory capacity ok and ready in protect mode...' ; 读取内存ok并准备进入保护模式msg信息打印
+    MSG_SETUP_LEN equ $ - setup_string_msg
+    get_mem_capacity_error_string_msg db 'get memory capacity failed!'        ; 读取内存失败并挂起计算机msg信息打印
+    MSG_GET_MEM_CAPACITY_ERROR_LEN equ $ - get_mem_capacity_error_string_msg
+
+    ;----------------------------------------------- 代码区域 ----------------------------------------------------
+
+; 获取内存尺寸并准备进入保护模式
 setup_start:
     mov sp,STACK_TOP_ADDR
     ; 获取内存分布（实模式）：后续进入保护模式内核需要(三种方法都使用)
+
     ; 1.使用bios的中断0x15子功能0xE820获取物理内存
     sub ebx,ebx
     mov di,Ards_buff
@@ -53,7 +62,7 @@ one_get_phys_mem:
     mov eax,0xe820                  
     mov ecx,ARDS_SIZE
     int 0x15
-    jc  two_get_phys_mem                ; 调用无错误执行下面代码
+    jc  two_get_phys_mem                ; 调用出错跳转到第二种获取物理内存的方法
     mov eax,[di]                        ; 保存获取到的最新内存容量
     add eax,[di + 8]   
     mov ecx,[Physical_memory_capacity]  ; 保存当前最大物理内存到ecx
@@ -68,21 +77,58 @@ is_over:                                ; 判断是否为最后一个结构体
     cmp ebx,0
     jnz one_get_phys_mem
     jmp get_mem_ok
+
     ; 2.使用bios的中断0x15子功能0xE801获取物理内存
 two_get_phys_mem:
-    mov bp,two_get_mem_string
-    mov cx,14
+    mov eax,0xe801
+    int 0x15
+    jc  three_get_phys_mem              ; 失败则调用三号方法获取物理内存
+    mov cx,0x400
+    mul cx                              ; 这里转成字节数的方法是因为mul的原因
+    shl edx,16
+    and eax,0x0000ffff
+    or  edx,eax
+    add edx,0x100000
+    mov esi,edx         
+
+    sub eax,eax
+    mov ax,bx
+    mov ecx,0x10000
+    mul ecx
+    
+    add esi,eax
+    mov [Physical_memory_capacity],esi  ; 得到的内存容量放置到Physical_memory_capacity
+    jmp get_mem_ok
+
+    ; 3.使用bios的中断0x15子功能0x88获取物理内存:只能获取小于64M的内存
+three_get_phys_mem:                 
+    mov ah,0x88
+    int 0x15
+    jc  get_mem_error
+    mov cx,0x400
+    mul cx
+    shl edx,16
+    and eax,0x0000ffff
+    or  edx,eax
+    add edx,0x100000 
+    mov [Physical_memory_capacity],edx    
+    jmp get_mem_ok
+    
+    ; 三种方法都无法获取内存容量，计算机挂起
+get_mem_error:                          
+    ; 显示读取内存失败msg信息：并且挂起计算机
+    mov bp,get_mem_capacity_error_string_msg
+    mov cx,MSG_GET_MEM_CAPACITY_ERROR_LEN
     mov ax,0x1301
     mov bx,0x001f
     mov dx,0x1700
-    int 0x10    
-    jmp $
+    int 0x10 
+    jmp $   
 
 get_mem_ok:                             ; 读取物理内存OK
-
     ; 显示msg信息：表示准备进入保护模式
     mov bp,setup_string_msg
-    mov cx,24
+    mov cx,MSG_SETUP_LEN
     mov ax,0x1301
     mov bx,0x001f
     mov dx,0x1800
